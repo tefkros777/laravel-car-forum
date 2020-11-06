30f628ec5fdd9964c69f010687569a033bb4a782
tefkros <constantinoslo98@gmail.com>
2020-11-06 21:14:41 +0000

    Add /home route

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,7 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/home', function () {
+    return 'Welcome to the largest car troubleshooting community, Jeremy Clarkson aprooved!';
+});

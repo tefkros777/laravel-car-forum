54b5a66d91435a310015f2c8f68d2962ca737316
tefkros <constantinoslo98@gmail.com>
2020-11-22 19:51:52 +0000

    Add app.blade.php layout

---
+++ resources/views/layouts/app.blade.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html>
+
+    <head>
+        <title>What's wrong with my car? - @yield('title')</title>
+    </head>
+    
+    <body>    
+        <h1>What's wrong with my car? - @yield('title')</h1>
+
+        <div>
+            @yield('content')
+        </div>
+
+    </body>
+    
+</html>

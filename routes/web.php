d904efdc04dd7bcd2a52a4cfd67ddaeb5efa9e9b
tefkros <constantinoslo98@gmail.com>
2020-12-17 00:29:16 +0000

    Redirect logo to posts.index

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use Illuminate\Support\Facades\Auth;
 
 /*
 |--------------------------------------------------------------------------
@@ -14,7 +15,8 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    // return view('welcome');
+    return redirect()->route('posts.index');
 });
 
 Route::get('/home', function () {

17d20f7c0ceb17cb2d021179761d978cda0baf5a
tefkros <constantinoslo98@gmail.com>
2020-12-21 22:48:57 +0200

    Formatted User details form (user.show)

--- resources/views/users/show.blade.php
+++ resources/views/users/show.blade.php
@@ -4,16 +4,20 @@
 
 @section('content')
 
-<ul>
-    <li><b>User ID:</b> {{ $user->id }}</li>
-    <li><b>Name:</b> {{$user->name }}</li>
-    <li><b>email:</b> {{ $user->email }}</li>
-</ul>
+<h3>User details</h3>
+
+<div class="card bg-light mb-3">
+    <ul class="list-group list-group-flush">
+        <li class="list-group-item"><b>User ID:</b> {{ $user->id }}</li>
+        <li class="list-group-item"><b>Name:</b> {{$user->name }}</li>
+        <li class="list-group-item"><b>Email (username):</b> {{ $user->email }}</li>
+    </ul>
+</div>
 
 <form action="{{ route('users.destroy', ['id' => $user->id]) }}" method="POST">
     @method('DELETE')
     @csrf
-    <button>Delete User</button>
+    <button class="btn btn-danger">Delete User</button>
 </form>
 
 @endsection

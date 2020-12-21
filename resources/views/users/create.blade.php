718fcd4883f3c7b39d446e7423469cabe17eb8aa
tefkros <constantinoslo98@gmail.com>
2020-12-21 22:41:24 +0200

    Formatted New user form

--- resources/views/users/create.blade.php
+++ resources/views/users/create.blade.php
@@ -4,18 +4,29 @@
 
 @section('content')
 
+<h3>New User</h3>
+
 <form method="POST" action="{{ route('users.store') }}">
     @csrf
+    <div class="card bg-light mb-3">
+        {{-- Name --}}
+        <div class="card-header">
+            <input class="border-0" type="text" name="name" placeholder="Display name"
+                value="{{ old('name') }}">
+        </div>
+        {{-- Email --}}
+        <div class="card-body">
+            <div style="float: left; width: 100%;">
+                <input class="border-0" type="text" name="email" placeholder="Email address"
+                value="{{ old('email') }}">
+            </div>
+        </div>
 
-    <p>Display name: <input type="text" name="name"
-        value="{{ old('name') }}"></p>
-
-    <p>Email address: <input type="text" name="email"
-        value="{{ old('email') }}"></p>
-
-    <input type="submit" value="Submit">
-
-    <a href="{{ route('users.index') }}">Cancel</a>
+        <div class="card-footer">
+            <input type="submit" value="Submit" class="btn btn-primary">
+            <a class="btn btn-secondary" href="{{ route('users.index') }}">Cancel</a>
+        </div>
+    </div>
 
 </form>
 

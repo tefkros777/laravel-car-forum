9ba081cd4bfc1920cbd539a1ef50a9081a9aa5c8
tefkros <constantinoslo98@gmail.com>
2021-01-02 02:22:56 +0200

    Add profile pic to Users.show

--- resources/views/users/show.blade.php
+++ resources/views/users/show.blade.php
@@ -3,23 +3,28 @@
 @section('title', 'User details')
 
 @section('content')
+    <div class="card bg-light mx-auto" style="width: 22rem;">
+        <div class="card-header">
+            <h5>Details</h5>
+        </div>
+        @if ($user->image)
+            <img class="card-img-top" src="{{ asset($user->image) }}" alt="Card image cap">
+        @endif
+        <ul class="list-group list-group-flush">
+            <li class="list-group-item"><b>User ID:</b> {{ $user->id }}</li>
+            <li class="list-group-item"><b>Name:</b> {{ $user->name }}</li>
+            <li class="list-group-item"><b>Email (username):</b> {{ $user->email }}</li>
+        </ul>
+        <div class="card-footer">
+            @if ($user->id == Auth::user()->id || Auth::user()->isAdmin())
+                <form action="{{ route('users.destroy', ['id' => $user->id]) }}" method="POST">
+                    @method('DELETE')
+                    @csrf
+                    <button class="btn btn-danger">Delete User</button>
+                </form>
+            @endif
+        </div>
 
-<h3>User details</h3>
-
-<div class="card bg-light mb-3">
-    <ul class="list-group list-group-flush">
-        <li class="list-group-item"><b>User ID:</b> {{ $user->id }}</li>
-        <li class="list-group-item"><b>Name:</b> {{$user->name }}</li>
-        <li class="list-group-item"><b>Email (username):</b> {{ $user->email }}</li>
-    </ul>
-</div>
-
-@if($user->id == Auth::user()->id || Auth::user()->isAdmin())
-    <form action="{{ route('users.destroy', ['id' => $user->id]) }}" method="POST">
-        @method('DELETE')
-        @csrf
-        <button class="btn btn-danger">Delete User</button>
-    </form>
-@endif
+    </div>
 
 @endsection
